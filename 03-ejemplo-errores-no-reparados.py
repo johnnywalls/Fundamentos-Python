fb918b155f6dc7baec39409d09aa4b151cc1a99c
Juan Paredes <juan.paredes@gmail.com>
2015-10-09 12:12:44 -0430

    Ejemplo de errores no reparados en programa Python

---
+++ 03-ejemplo-errores-no-reparados.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# Inicializar variables
+f = c = 0
+
+# Fórmula de Conversión de Celsius a Fahrenheit
+f + 32 = 9/5 * c
+
+c = float(input('Introduzca la temperatura en °C (centígrados):')
+
+print "La temperatura equivalente en °F es", c
